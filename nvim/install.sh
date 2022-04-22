6539ec1511b5e8141346c5bdc3569a053d9671d2
Agus Prayogi <agus21apy@gmail.com>
2022-04-22 22:00:14 +0700

    :hammer: perubahan

--- nvim/install.sh
+++ nvim/install.sh
@@ -14,4 +14,6 @@ mkdir -p ${HOME}/.config/nvim
 echo_info "Symlink init.vim..."
 ln -sfT "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
+curl -sLf https://spacevim.org/install.sh | bash
+
 echo_done "Nvim configuration!"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -25,8 +25,9 @@ if ! zgen saved; then
   zgen load zsh-users/zsh-completions src
 
   # theme
-  # zgen oh-my-zsh themes/robbyrussell
-  zgen load denysdovhan/spaceship-prompt spaceship
+  zgen load romkatv/powerlevel10k powerlevel10k
+  # zgen oh-my-zsh themes/duellj
+  # zgen load denysdovhan/spaceship-prompt spaceship
 
   # save all to init script
   zgen save
@@ -42,28 +43,28 @@ SPACESHIP_PROMPT_ORDER=(
 #   # hg        # Mercurial section (hg_branch  + hg_status)
   package   # Package version
   node      # Node.js section
-#   # ruby      # Ruby section
+  ruby      # Ruby section
 #   # elixir    # Elixir section
 #   # xcode     # Xcode section
 #   # swift     # Swift section
-#   # golang    # Go section
+  golang    # Go section
   php       # PHP section
 #   # rust      # Rust section
 #   # haskell   # Haskell Stack section
 #   # julia     # Julia section
   docker    # Docker section
-#   # aws       # Amazon Web Services section
-#   # venv      # virtualenv section
+  aws       # Amazon Web Services section
+  venv      # virtualenv section
 #   # conda     # conda virtualenv section
 #   # pyenv     # Pyenv section
-#   # dotnet    # .NET section
-#   # ember     # Ember.js section
-#   # kubectl   # Kubectl context section
+  dotnet    # .NET section
+  ember     # Ember.js section
+  kubectl   # Kubectl context section
 #   # terraform # Terraform workspace section
 #   # exec_time # Execution time
 #   # line_sep  # Line break
 #   battery   # Battery level and status
-#   # vi_mode   # Vi-mode indicator
+  vi_mode   # Vi-mode indicator
 #   # jobs      # Background jobs indicator
 #   # exit_code # Exit code section
   char      # Prompt character
