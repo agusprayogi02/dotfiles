8370e851dbcbceacaa8d84da4598eeb881251f58
Agus Prayogi <agus21apy@gmail.com>
2023-09-03 14:25:54 +0700

    [ADD] php config dnf fedora

---
+++ php-fedora/Dnf.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Valet\PackageManagers;
+
+use DomainException;
+use Valet\CommandLine;
+use Valet\Contracts\PackageManager;
+
+class Dnf implements PackageManager
+{
+    public $cli;
+    public $redisPackageName = 'redis';
+    public $mysqlPackageName = 'mysql-server';
+    public $mariaDBPackageName = 'mariadb-server';
+
+    const PHP_FPM_PATTERN_BY_VERSION = [];
+
+    /**
+     * Create a new Apt instance.
+     *
+     * @param CommandLine $cli
+     *
+     * @return void
+     */
+    public function __construct(CommandLine $cli)
+    {
+        $this->cli = $cli;
+    }
+
+    /**
+     * Determine if the given package is installed.
+     *
+     * @param string $package
+     *
+     * @return bool
+     */
+    public function installed($package)
+    {
+        $package = $this->getPhpPackage($package);
+        $query = "dnf list installed {$package} | grep {$package} | sed 's_  _\\t_g' | sed 's_\\._\\t_g' | cut -f 1";
+        $packages = explode(PHP_EOL, $this->cli->run($query));
+
+        return in_array($package, $packages);
+    }
+
+    /**
+     * Ensure that the given package is installed.
+     *
+     * @param string $package
+     *
+     * @return void
+     */
+    public function ensureInstalled($package)
+    {
+        $package = $this->getPhpPackage($package);
+        if (!$this->installed($package)) {
+            $this->installOrFail($package);
+        }
+    }
+
+    /**
+     * Install the given package and throw an exception on failure.
+     *
+     * @param string $package
+     *
+     * @return void
+     */
+    public function installOrFail($package)
+    {
+        $package = $this->getPhpPackage($package);
+        output('<info>['.$package.'] is not installed, installing it now via Dnf</info>');
+
+        $this->cli->run(trim('dnf install -y '.$package), function ($exitCode, $errorOutput) use ($package) {
+            output($errorOutput);
+
+            throw new DomainException('Dnf was unable to install ['.$package.'].');
+        });
+    }
+
+    /**
+     * Configure package manager on valet install.
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        // Nothing to do
+    }
+
+    /**
+     * Restart dnsmasq in Fedora.
+     */
+    public function nmRestart($sm)
+    {
+        $sm->restart('NetworkManager');
+    }
+
+    /**
+     * Determine if package manager is available on the system.
+     *
+     * @return bool
+     */
+    public function isAvailable()
+    {
+        try {
+            $output = $this->cli->run('which dnf', function ($exitCode, $output) {
+                throw new DomainException('Dnf not available');
+            });
+
+            return $output != '';
+        } catch (DomainException $e) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine php fpm package name.
+     *
+     * @return string
+     */
+    public function getPhpFpmName($version)
+    {
+        $pattern = !empty(self::PHP_FPM_PATTERN_BY_VERSION[$version])
+            ? self::PHP_FPM_PATTERN_BY_VERSION[$version] : 'php{VERSION}-fpm';
+
+        return str_replace('{VERSION}', $version, $pattern);
+    }
+
+    /**
+     * Determine php extension pattern.
+     *
+     * @return string
+     */
+    public function getPhpExtensionPattern($version)
+    {
+        return 'php{VERSION}';
+    }
+
+    public function getPhpPackage($package)
+    {
+        $all = explode('-', $package);
+        if($all > 1){
+            if(stripos($all[0], 'php') !== false){
+                $package = 'php' . '-' . $all[1];
+                if($package == 'php-mysql'){
+                    $package = 'php-mysqlnd';
+                }
+            }
+        }
+        return $package;
+    }
+}
+
